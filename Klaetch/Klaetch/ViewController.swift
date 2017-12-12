854305661dc3bfd4ad20a2f889a0f2d205748bd9
xeliot <daveho8888@gmail.com>
2017-12-11 22:20:22 -0800

    initial commit

---
+++ Klaetch/Klaetch/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Klaetch
+//
+//  Created by Dave Ho on 12/11/17.
+//  Copyright © 2017 ZetaPhase Tech. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
