1e40ff1feadbd073620eb6cc427f735519b468e5
xeliot <daveho8888@gmail.com>
2017-12-28 10:03:10 -0800

    add class file for create party view controller

---
+++ Klaetch/Klaetch/CreatePartyViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CreatePartyViewController.swift
+//  Klaetch
+//
+//  Created by Dave Ho on 12/28/17.
+//  Copyright © 2017 ZetaPhase Tech. All rights reserved.
+//
+
+import UIKit
+
+class CreatePartyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
