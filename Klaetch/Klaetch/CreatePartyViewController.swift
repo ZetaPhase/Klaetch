443df66007e678708568afc20e763b20031809f1
xeliot <daveho8888@gmail.com>
2018-01-03 20:08:48 -0800

    add mapkitmklocalsearchcompleter

--- Klaetch/Klaetch/CreatePartyViewController.swift
+++ Klaetch/Klaetch/CreatePartyViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import MapKit
 
 class CreatePartyViewController: UIViewController {
 
+    var searchCompleter = MKLocalSearchCompleter()
+    var searchResults = [MKLocalSearchCompletion]()
+    
+    @IBOutlet weak var searchResultsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         self.showAnimate()
+        
+        searchCompleter.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,3 +55,68 @@ class CreatePartyViewController: UIViewController {
         });
     }
 }
+
+extension CreatePartyViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        searchCompleter.queryFragment = searchText
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchResultsTableView.alpha = 1.0
+    }
+}
+
+extension CreatePartyViewController: MKLocalSearchCompleterDelegate {
+    
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        searchResults = completer.results
+        searchResultsTableView.reloadData()
+    }
+    
+    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
+        // handle error
+    }
+}
+
+extension CreatePartyViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let searchResult = searchResults[indexPath.row]
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        cell.textLabel?.text = searchResult.title
+        cell.detailTextLabel?.text = searchResult.subtitle
+        return cell
+    }
+}
+
+extension CreatePartyViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        print("hi")
+        print(searchResultsTableView.alpha)
+        searchResultsTableView.alpha = 0.0
+        print(searchResultsTableView.alpha)
+        
+        let completion = searchResults[indexPath.row]
+        
+        let searchRequest = MKLocalSearchRequest(completion: completion)
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { (response, error) in
+            let coordinate = response?.mapItems[0].placemark.coordinate
+            print(String(describing: coordinate))
+        }
+    }
+}
+
